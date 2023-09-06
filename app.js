51ac3e3cd4182054ef22c2c8e5d2c822147b28c2
Tanner Davison <Tboydavison@gmail.com>
2023-09-05 20:07:32 -0600

    added sequelize and updated app.js to use sequelize and synced db to all table models

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
+const sequelize = require('./util/database')
 
 const app = express();
 
@@ -23,4 +23,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+sequelize.sync().then(result=>{
 app.listen(3000);
+})
+    .catch(err=>{
+    console.log(err)
+})
+
+
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: 'Luliann465$'
-});
-
-module.exports = pool.promise();
+const sequelize = new Sequelize(
+  "node_complete", "root", "Luliann465$", {
+     dialect: "mysql",
+     host: 'localhost'
+    });
+    module.exports = sequelize;

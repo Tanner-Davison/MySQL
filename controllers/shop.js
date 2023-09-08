4806acbfc3b0d2ce0da7044929f09196cc70b7d8
Tanner Davison <Tboydavison@gmail.com>
2023-09-07 20:38:21 -0600

    finished sequelize logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 const Cart = require("../models/cart");
-
+const Order = require("../models/order");
 exports.getProducts = (req, res, next) => {
   Product.findAll()
     .then((products) => {
@@ -90,7 +90,7 @@ exports.postCart = (req, res, next) => {
         newQuantity = oldQuantity + 1;
         return product;
       }
-      return Product.findById(prodId);
+      return Product.findByPk(prodId);
     })
     .then((product) => {
       return fetchedCart.addProduct(product, {
@@ -120,16 +120,56 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .then((result) => {
+          return fetchedCart.setProducts(null);
+        })
+        .then((result) => {
+          res.redirect("/orders");
+        })
+        .catch((err) => console.log(err));
+    });
+};
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+  .getOrders({include: ['products']})
+    .then(orders =>{
+       res.render("shop/orders", {
+         path: "/orders",
+         pageTitle: "Your Orders",
+         orders: orders
+       });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+ 
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
-};
+// exports.getCheckout = (req, res, next) => {
+//   res.render("shop/checkout", {
+//     path: "/checkout",
+//     pageTitle: "Checkout",
+//   });
+// };
--- models/order-item.js
+++ models/order-item.js
@@ -1,15 +1,17 @@
-const Sequelize = require("sequelize");
+const DataTypes = require("sequelize");
 
 const sequelize = require("../util/database");
 
 const OrderItem = sequelize.define("orderItem", {
   id: {
-    type: Sequelize.INTEGER,
+    type: DataTypes.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  quantity: Sequelize.INTEGER,
+ quantity: {
+  type: DataTypes.INTEGER,
+ }
 });
 
 module.exports = OrderItem;
